04a181a7faceeabdf6d277377e7b0dc667d56269
Zaid Shaikh <zaid.shaikh@kanini.com>
2026-03-11 13:05:47 +0530

    backend issues fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // API Configuration
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://temp-repo-i80r.onrender.com'
+const API_BASE_URL ='https://temp-repo-ickb.onrender.com'
 export default API_BASE_URL
